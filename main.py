7738e3f1d3a764e77323104a05d3306faa2be187
Serhii Blyzniuk <155555957+Serg78935@users.noreply.github.com>
2025-03-31 12:13:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from core.backtester import Backtester
+from strategies.sma_cross import SMACrossover  # або іншу стратегію
+from core.data_loader import DataLoader
+
+# Завантаження даних
+data = DataLoader.load("data/btc_1m_feb25.parquet")
+
+# Запуск бектесту
+strategy = SMACrossover()
+backtester = Backtester(data, strategy)
+results = backtester.run()
+
+# Збереження результатів
+results.to_csv("results/metrics.csv")
+print("Backtest завершено, результати збережено у results/metrics.csv")
