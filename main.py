5962557b68b264bf63d73c7f0b50a52af03f03e0
Serhii Blyzniuk <155555957+Serg78935@users.noreply.github.com>
2025-03-31 21:07:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+"""
 from core.backtester import Backtester
 from strategies.sma_cross import SMACrossover  # або іншу стратегію
 from core.data_loader import DataLoader
@@ -7,6 +7,12 @@ from core.data_loader import DataLoader
 loader = DataLoader()
 df = loader.load()
 print(df.head())  # Переконайтесь, що дані є
+"""
+import pandas as pd
+
+df = pd.read_parquet("data/btc_1m_2025-02.parquet")
+print(df.head())
+
 
 """
 # Завантаження даних
