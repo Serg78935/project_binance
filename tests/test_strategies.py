29cfd9e3bf02a95aa771b633b6bbcbdfad33940a
Serhii Blyzniuk <155555957+Serg78935@users.noreply.github.com>
2025-03-31 16:57:58 +0300

    Update test_strategies.py

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -14,15 +14,24 @@ dummy_data = pd.DataFrame({
 
 def test_sma_cross():
     strategy = SMACrossover(price_data=dummy_data)
-    assert strategy is not None
+    data = pd.DataFrame({'close': [100, 101, 102, 103, 104]})
+    signal = strategy.generate_signal(data)
+    assert signal in [-1, 0, 1], "Signal should be -1, 0, or 1"
+    #assert strategy is not None
 
 def test_rsi_bb():
     strategy = RSI_Bollinger(price_data=dummy_data)
-    assert strategy is not None
+    data = pd.DataFrame({'close': [100, 101, 102, 103, 104]})
+    signal = strategy.generate_signal(data)
+    assert signal in [-1, 0, 1], "Signal should be -1, 0, or 1"
+    #assert strategy is not None
 
 def test_vwap_reversion():
     strategy = VWAPReversion(price_data=dummy_data)
-    assert strategy is not None
+    data = pd.DataFrame({'close': [100, 101, 102, 103, 104]})
+    signal = strategy.generate_signal(data)
+    assert signal in [-1, 0, 1], "Signal should be -1, 0, or 1"
+    #assert strategy is not None
 """
 def test_sma_cross():
     strategy = SMACrossover()
