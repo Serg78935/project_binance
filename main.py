53158d3452f3780c8b4aae0cdb8403977c23e85a
Serhii Blyzniuk <155555957+Serg78935@users.noreply.github.com>
2025-04-01 04:13:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,17 @@ from core.backtester import Backtester
 from strategies.sma_cross import SMACrossover  # або іншу стратегію
 from core.data_loader import DataLoader
   
-"""
+
 loader = DataLoader()
 df = loader.load()
 print(df.head())  # Переконайтесь, що дані є
 
-import pandas as pd
-
-df = pd.read_parquet("data/btc_1m_2025-02.parquet")
-print(df.head())
 """
-
-# Завантаження даних
-#data = DataLoader.load("data/btc_1m_2025-02.parquet")
-
 # Завантаження даних
 import pandas as pd
 file_path = "data/btc_1m_2025-02.parquet"
 df = pd.read_parquet(file_path)
-
+"""
 # Запуск бектесту
 strategy = SMACrossover(df)
 backtester = Backtester(strategy)
