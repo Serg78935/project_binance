691395079b00cc8ea4232a3580000fa4cc2c12e2
Serhii Blyzniuk <155555957+Serg78935@users.noreply.github.com>
2025-04-18 20:48:58 +0300

    Update main.py
    update file

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 
+import time
 from core.backtester import Backtester
-from strategies.sma_cross import SMACrossover  # або іншу стратегію
+from strategies.sma_cross import SMACrossover  
+from strategies.rsi_bb import RSI_Bollinger
+from strategies.vwap_reversion import VWAPReversion
 from core.data_loader import DataLoader
-  
 
+# Завантаження даних
 loader = DataLoader()
 df = loader.load()
+
 print(df.head())  # Переконайтесь, що дані є
 
-"""
-# Завантаження даних
-import pandas as pd
-file_path = "data/btc_1m_2025-02.parquet"
-df = pd.read_parquet(file_path)
-"""
-# Запуск бектесту
-strategy = SMACrossover(df)
-backtester = Backtester(strategy)
-results = backtester.run()
+# Стратегії
+backtesters = [
+    Backtester(df, SMACrossover, "sma_cross"),
+    Backtester(df, RSI_Bollinger, "rsi_bb"),
+    Backtester(df, VWAPReversion, "vwap_reversion")
+]
 
-# Збереження результатів
-results.to_csv("results/metrics.csv")
+print("Starting backtest...")
+start_time = time.time()
+
+for bt in backtesters:
+    bt.run()
+
+# Запуск бектесту
+backtester = Backtester( df, SMACrossover, "sma_cross") 
+
+backtester.compare_strategies_full([
+    "results/metrics_sma_cross.csv",
+    "results/metrics_rsi_bb.csv",
+    "results/metrics_vwap_reversion.csv"
+])
+
+
+end_time = time.time()
+#print(f"Backtest completed for ETHBTC in {end_time - start_time} seconds.")
+
 print("Backtest завершено, результати збережено у results/metrics.csv")
+
+
 
